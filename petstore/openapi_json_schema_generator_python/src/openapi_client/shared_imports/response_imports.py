edd901668f15c4fc8011767398252f9e5ab1addc
Justin Black <justin.a.black@gmail.com>
2023-05-09 16:54:56 -0700

    Sample regen

---
+++ petstore/openapi_json_schema_generator_python/src/openapi_client/shared_imports/response_imports.py
@@ -0,0 +1,12 @@
+import dataclasses
+import datetime
+import decimal
+import io
+import typing
+import uuid
+
+import frozendict
+import typing_extensions
+import urllib3
+
+from openapi_client import api_client, schemas
---
+++ petstore/openapi_json_schema_generator_python/src/openapi_client/shared_imports/server_imports.py
@@ -0,0 +1,5 @@
+import dataclasses
+import typing
+import typing_extensions
+
+from openapi_client import server, schemas
